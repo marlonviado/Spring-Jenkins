235d52e0b1c9bf1d9a1784765a045eae69398096
Viado <marlon.viado@accenture.com>
2023-03-17 12:54:11 +0800

    Third commit

--- src/test/java/com/marlonviado/SpringJenkinsApplicationTests.java
+++ src/test/java/com/marlonviado/SpringJenkinsApplicationTests.java
@@ -1,13 +1,19 @@
 package com.marlonviado;
 
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
+	
+	public static Logger logger = (Logger) LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals(true, true);
 	}
 
 }
