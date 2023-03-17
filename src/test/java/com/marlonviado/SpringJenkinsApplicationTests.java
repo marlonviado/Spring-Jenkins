3e0bf4d0d9765f3a03f1033d48f5cd7831e7dc70
Viado <marlon.viado@accenture.com>
2023-03-17 12:12:04 +0800

    First commit

---
+++ src/test/java/com/marlonviado/SpringJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlonviado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
