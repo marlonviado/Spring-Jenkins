b81e431ae6616b2d1057549a48ed77b27eca57b5
Viado <marlon.viado@accenture.com>
2023-03-17 13:05:30 +0800

    Fix logger

--- src/test/java/com/marlonviado/SpringJenkinsApplicationTests.java
+++ src/test/java/com/marlonviado/SpringJenkinsApplicationTests.java
@@ -2,11 +2,8 @@ package com.marlonviado;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 	
