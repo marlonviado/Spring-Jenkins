980481aa8e66177cd65ea1ba62245179ed68f94d
marlon viado <mfviado@hotmail.com>
2026-05-08 23:57:12 +0800

    Fix pom file

--- src/main/java/com/marlonviado/service/PaymentService.java
+++ src/main/java/com/marlonviado/service/PaymentService.java
@@ -1,12 +1,14 @@
 package com.marlonviado.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import com.marlonviado.dto.Order;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
-@Slf4j
 public class PaymentService {
+	
+	private static final Logger log = LoggerFactory.getLogger(PaymentService.class);
 	
 	public Order processPayment(Order order) throws InterruptedException {
 		Thread.sleep(2000);
